1d962a77f04caa99ade3506a087da25a38b91d28
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-09-03 23:53:08 +0200

    Add files via upload

--- Student.java
+++ Student.java
@@ -5,4 +5,30 @@ package SMS;
 students fess, name, ID, Grade and fees paid
  */
 public class Student {
+    private int id;
+    private String name;
+    private int grade;
+    private int feesPaid;
+    private int feesTotal;
+
+/* Constructor : the role of a constructor is to
+create a new object ( here the object is student )
+by initializing the values
+ */
+
+    /**
+     *
+     * @param id id for the student : unique.
+     * @param name : name of the student.
+     * @param grade : grade of the student.
+     */
+    public Student(int id, String name,int grade){
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+        
+
+}
+
+
 }
